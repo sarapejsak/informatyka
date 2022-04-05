ce01d92efa855163e1093520327fffae22baa711
Sara Pejsak <kasjeparas@gmail.com>
2022-04-05 10:17:38 +0200

    liczby pierwsze

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,31 @@
+﻿// ConsoleApplication1.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n = 0;
+	cout << "ile liczb pierwszych chcesz wyświetlić: ";
+	cin >> n;
+	int lp = 0;
+	int p = 2;
+	bool jakos;
+
+	while (lp < n) {
+		jakos = true;
+		for (int d = 2; d < p - 1; d++) {
+			if (p % d == 0)
+				jakos = false;
+		}
+		if (jakos == true) {
+			cout << p << " ";
+			lp += 1;
+		}
+		p += 1;
+	}
+
+}
